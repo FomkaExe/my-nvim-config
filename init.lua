083af048ae83578c74d51348f363359091867c75
FomkaExe <fomkaexe@yandex.ru>
2025-05-27 08:31:40 +0300

    Add mini.nvim modules

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
--- https://vonheikemen.github.io/devlog/tools/build-your-first-lua-config-for-neovim/
--- Props to that dude
+-- Vim options
 
 vim.o.number = true
 vim.o.wrap = true
@@ -11,7 +10,7 @@ vim.o.hlsearch = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 
-vim.o.showmode = true --?
+vim.o.showmode = true
 
 vim.o.termguicolors = true
 
@@ -20,15 +19,20 @@ vim.o.timeoutlen = 300
 
 vim.o.signcolumn = 'yes'
 
-vim.g.mapleader = ' '
+vim.g.mapleader = vim.keycode('<Space>')
 vim.keymap.set({'n', 'x'}, 'gy', '"+y', {desc = 'Copy to clipboard'})
 vim.keymap.set({'n', 'x'}, 'gp', '"+p', {desc = 'Paste clipboard content'})
+
+-- Colour theme
 
 local ok_theme = pcall(vim.cmd.colorscheme, 'tokyonight')
 if not ok_theme then
 	vim.cmd.colorscheme('habamax')
 end
 
+-- Plugins
+
+-- -- mini.nvim setup
 require('mini.files').setup({})
 vim.keymap.set('n', '<leader>e', '<cmd>lua MiniFiles.open()<cr>', {desc='File Explorer'})
 
@@ -38,42 +42,35 @@ require('mini.icons').setup({})
 require('mini.starter').setup({
 	header = 'Emacs',
 })
+require('mini.comment').setup({
+	options = {
+		custom_commentstring = nil,
+		ignore_blank_line = false,
+		start_of_line = false,
+		pad_comment_parts = true,
+	},
+	mappings = {
+		comment = '<leader>c',
+		comment_line = '<leader>cc',
+		comment_visual = '<leader>c',
+		textobject = '<leader>c',
+	},
+	hooks = {
+		pre = function() end,
+		post = function() end,
+	},
+})
 
--- I don't know if I need it
---[[ vim.api.nvim_create_autocmd('LspAttach', {
-	desc = 'LSP actions',
-	callback = function(event)
-		local opts = {buffer = event.buf}
+-- -- clangd setup
 
-		-- Display documentation of the symbol under the cursor
-		vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+vim.lsp.enable('clangd')
+require("clangd_extensions").setup({})
 
-		-- Jump to the definition
-		vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+-- -- clangd keybinds
 
-		-- Format current file
-		vim.keymap.set({'n', 'x'}, 'gq', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+vim.keymap.set('n', '<leader>d', '<cmd>ClangdSwitchSourceHeader<cr>', {desc='Goto definition'})
 
-		-- Displays a function's signature information
-		vim.keymap.set('i', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+vim.keymap.set('n', '<leader>a', '<cmd>ClangdAST<cr>', {desc='Abstract Syntax Tree'})
+vim.keymap.set('x', '<leader>a', '<cmd>\'<,\'>ClangdAST<cr>', {desc='Abstract Syntax Tree'})
 
-		-- Jump to declaration
-		vim.keymap.set('n', '<leader>ld', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-
-		-- Lists all the implementations for the symbol under the cursor
-		vim.keymap.set('n', '<leader>li', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-
-		-- Jumps to the definition of the type symbol
-		vim.keymap.set('n', '<leader>lt', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-
-		-- Lists all the references
-		vim.keymap.set('n', '<leader>lr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-
-		-- Renames all references to the symbol under the cursor
-		vim.keymap.set('n', '<leader>ln', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-
-		-- Selects a code action available at the current cursor position
-		vim.keymap.set('n', '<leader>la', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-	end,
-	})
-} ]]
+vim.keymap.set('n', '<leader>t', '<cmd>ClangdTypeHierarchy<cr>', {desc='Type hierarchy'})
